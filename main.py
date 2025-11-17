6a7204a5e9fc3b42efb683c5dbc1eb8ad3dc0315
naghamthamer <naghamalotaibi@gmail.com>
2025-11-17 22:37:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import FileResponse
+import requests
+from fastapi import FastAPI, Form
 from pdf2image import convert_from_bytes
 import pytesseract
+from fastapi.responses import FileResponse
 import os
-import uvicorn
-
-
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 app = FastAPI()
-@app.post("/search-word/")
-async def search_word_in_pdf(
-    file: UploadFile = File(...),
+
+@app.post("/search-word-url/")
+async def search_word_in_pdf_url(
+    file_url: str = Form(...),
     search_word: str = Form(...)
 ):
-    pdf_bytes = await file.read()
+    # جلب الملف من SharePoint
+    response = requests.get(file_url)
+    pdf_bytes = response.content
+
+    # تحويل PDF إلى صور
     images = convert_from_bytes(pdf_bytes)
     matching_pages = []
 
@@ -22,14 +24,10 @@ async def search_word_in_pdf(
         text = pytesseract.image_to_string(page)
         if search_word.lower() in text.lower():
             matching_pages.append(page)
-    if matching_pages:
-        output_pdf_path = os.path.join(f"matched_pages.pdf")
-        matching_pages[0].save(output_pdf_path, save_all=True, append_images=matching_pages[1:])
-        return FileResponse(
-            output_pdf_path,
-            media_type="application/pdf",
-            filename=f"matched_pages.pdf"
-        )
 
+    if matching_pages:
+        output_pdf_path = "matched_pages.pdf"
+        matching_pages[0].save(output_pdf_path, save_all=True, append_images=matching_pages[1:])
+        return FileResponse(output_pdf_path, media_type="application/pdf", filename="matched_pages.pdf")
     else:
         return {"message": f"No pages found containing '{search_word}'."}
